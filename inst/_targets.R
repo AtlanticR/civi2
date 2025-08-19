f83b02f1ceb5e7603aa837efd707cea440b1e384
remi-daigle <ENT\daigler@wpnsbio9039519.ent.dfo-mpo.ca>
2025-08-19 16:42:53 -0300

    target-ify sites, sea level, and ice days

--- inst/_targets.R
+++ inst/_targets.R
@@ -1,13 +1,16 @@
 if(!require(librarian)) install.packages("librarian")
 pkgs <- c("targets",
+          "readxl",
           "sf",
           "stars",
+          "ncmeta",
           "qs",
           "qs2",
           "dplyr")
 shelf(pkgs)
-tar_option_set(packages = pkgs)
+tar_option_set(packages = basename(pkgs))
 
+# Set the store path for targets
 if(dir.exists("//wpnsbio9039519.mar.dfo-mpo.ca/sambashare/CIVI/civi2")){
   store = "//wpnsbio9039519.mar.dfo-mpo.ca/sambashare/CIVI/civi2"
 } else if(dir.exists("/srv/sambashare/CIVI/civi2")){
@@ -20,12 +23,78 @@ if(dir.exists("//wpnsbio9039519.mar.dfo-mpo.ca/sambashare/CIVI/civi2")){
 tar_config_set(store = store)
 
 
-
+# Define the targets pipeline
 list(
+  # Raw Data
   tar_target(data_CIVI_Sites,
              command={
-               NULL
+               # file from Yanice Berkane (SCH)
+               read_excel(file.path(store, "data", "2025-08-19 - SCH Harbours - Latitude and Longitude.xlsx")) |>
+                 filter(!is.na(Latitude) & !is.na(Longitude)) |>
+                 sf::st_as_sf(coords = c("Longitude", "Latitude"),
+                          crs = 4326,
+                          remove = FALSE)
              }),
+
+  tar_target(data_sea_level,
+             command={
+               # the download url is the HTTPServer link from the link sent by Carrington Pomeroy (ECCC)
+               # https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/catalog/birdhouse/disk2/cccs_portal/indices/Final/SEA_LEVEL_RISE/catalog.html?dataset=birdhouse/disk2/cccs_portal/indices/Final/SEA_LEVEL_RISE/Decadal_CMIP6_ensemble-percentiles_allssps_2020-2150_rslc_uplift_YS.nc
+               download_url <- "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/fileServer/birdhouse/disk2/cccs_portal/indices/Final/SEA_LEVEL_RISE/Decadal_CMIP6_ensemble-percentiles_allssps_2020-2150_rslc_uplift_YS.nc"
+
+               # Download the file
+               temp_file <- tempfile(fileext = ".nc")
+               download.file(download_url, temp_file, mode = "wb")
+
+               read_ncdf(temp_file,var ="ssp245_rslc_p50") |>
+                 slice("time",9) |> # should be 2100
+                 st_set_crs(4326)
+             }),
+
+  tar_target(data_CIVI_ReplacementCost,
+             command={
+               # file from Annie Boudreau (SCH)
+               read_excel(file.path(store, "data", "2025-08-05 - CIVI Replacement Cost.xlsx"))
+             }),
+
+  tar_target(data_CIVI_HarbourCondition,
+             command={
+               # file from Annie Boudreau (SCH)
+               read_excel(file.path(store, "data", "2025-08-05 - CIVI Harbour Condition use and capacity.xlsx"))
+             }),
+
+
+  tar_target(data_ice_days,
+             command={
+               # data from:
+               # https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/catalog/datasets/simulations/bias_adjusted/cmip6/climatedata_ca/CanDCS-M6/ensemble_members/30yAvg/catalog.html?dataset=datasets/simulations/bias_adjusted/cmip6/climatedata_ca/CanDCS-M6/ensemble_members/30yAvg/annual_CanDCS-M6_climindices_ensemble_members_30yAvg.ncml
+               # THREDDS OPeNDAP URL for the climate indices dataset
+               url <- "https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/dodsC/datasets/simulations/bias_adjusted/cmip6/climatedata_ca/CanDCS-M6/ensemble_members/30yAvg/annual_CanDCS-M6_climindices_ensemble_members_30yAvg.ncml"
+
+               ssp245_ice_days <- read_ncdf(url, var = "ssp245_ice_days")
+
+               ssp245_ice_days_1971 <- st_apply(
+                 ssp245_ice_days[,,,, 3],
+                 c("lon", "lat"),
+                 mean,
+                 na.rm = TRUE
+               ) |>
+                 st_as_stars()
+
+               ssp245_ice_days_2071 <- st_apply(
+                 ssp245_ice_days[,,,, 13],
+                 c("lon", "lat"),
+                 mean,
+                 na.rm = TRUE
+               ) |>
+                 st_as_stars()
+
+               ice_days_delta <- ssp245_ice_days_2071 - ssp245_ice_days_1971
+
+               ice_days_delta
+             }),
+
+
 
   # Indicators
 
@@ -46,12 +115,15 @@ list(
 
   tar_target(ind_sea_level_change,
              command={
-               NULL
+               st_extract(data_sea_level, data_CIVI_Sites)
+               #TODO score 1-5, normalize? remove geometry add harbour code?
              }),
 
   tar_target(ind_ice_day_change,
              command={
-               NULL
+               st_extract(data_ice_days, data_CIVI_Sites)
+               #TODO score 1-5, normalize? remove geometry add harbour code?
+
              }),
 
   tar_target(ind_replacement_cost,
