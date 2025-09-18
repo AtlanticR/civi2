581c5019a46bc3ba2a18219e5702ba88aec71a37
remi-daigle <ENT\daigler@wpnsbio9039519.ent.dfo-mpo.ca>
2025-09-18 11:42:45 -0300

    indlude ors_api_key

--- R/ind_proximity.R
+++ R/ind_proximity.R
@@ -43,7 +43,7 @@
 #' @importFrom fasterize fasterize
 #' @importFrom gdistance transition geoCorrection shortestPath
 #' @importFrom ggplot2 ggplot geom_sf geom_text labs theme_minimal coord_sf
-#' @importFrom openrouteservice ors_directions
+#' @importFrom openrouteservice ors_directions ors_api_key
 #' @importFrom leaflet leaflet addTiles addPolylines addMarkers
 #'
 #' @export
